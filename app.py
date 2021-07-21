1c3165c39cc2f415c97ccfc29fedb786b35c9fef
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2021-07-21 17:45:50 +0700

    Init

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from inference import *
+
+deep_se_model = DeepSE(emb_weight_file, dict_file, model_file, max_len=MAX_LEN)
+
+if __name__ == '__main__':
+    st.image('./static/images/TPS_AI.png')
+    st.title('Story Point Estimator System')
+    st.write('This system takes input of title and description of an issue and estimates the number of story points required to complete it.')
+
+    title = st.text_input('Title (max length: 100)')
+    descr = st.text_area('Description (max length: 100)')
+
+    if st.button('Estimate'):
+        prediction = 0
+        print(descr)
+
+        with st.spinner(text='Estimating in progress...'):
+            prediction = deep_se_model.inference([title], [descr])[0][0]
+            st.success('Done')
+
+        st.header('Story points: %.1f' % prediction)
